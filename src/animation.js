bb593a8d9356155c0f07179bba7bba5ac1da5587
Nick Rasmussen <nlr30262g.rit.edu>
2020-10-13 21:01:06 -0400

    ayyyy I made vertices!!!

--- src/animation.js
+++ src/animation.js
@@ -4,11 +4,12 @@ class Vertex{
         this.y = y;
         if(parent){
             this.parent = parent;
-            //parent.AddChild(this);
+            parent.AddChild(this);
         }
+        this.children = [];
     }
     AddChild(vertex){
-        this.children.add(vertex);
+        this.children.push(vertex);
     }
 }
 
@@ -16,18 +17,30 @@ class AnimBody{
     constructor(){
         this.root = new Vertex(100,100);
         this.v1 = new Vertex(100,50,this.root);
+        this.v2 = new Vertex(50,100,this.v1);
+        this.v3 = new Vertex(200,100,this.v1);
     }
     
     draw(ctx){
+        this.drawVertex(this.root,ctx);
+    }
+    
+    drawVertex(v,ctx){
         ctx.save();
         ctx.fillStyle = "white";
+        ctx.strokeStyle = "white";
+        for(let i = 0; i < v.children.length; i++){
+            this.drawVertex(v.children[i],ctx);
+            ctx.beginPath();
+            ctx.moveTo(v.x,v.y);
+            ctx.lineTo(v.children[i].x,v.children[i].y);
+            ctx.stroke();
+            ctx.closePath();
+        }
         ctx.beginPath();
-        ctx.arc(this.root.x,this.root.y,10,0,Math.PI * 2);
+        ctx.arc(v.x,v.y,10,0,Math.PI * 2);
         ctx.fill();
         ctx.closePath();
-        ctx.beginPath();
-        ctx.arc(this.v1.x,this.v1.y,5,0,Math.PI * 2);
-        ctx.fill();
         ctx.restore();
     }
 }
