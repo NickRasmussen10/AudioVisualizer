be53ae1cf1d22060018c089b1f9cf30dacd536bf
Nick Rasmussen <nlr30262g.rit.edu>
2020-10-27 08:28:31 -0400

    I don't even know what reality is anymore

--- src/animation.js
+++ src/animation.js
@@ -1,7 +1,5 @@
 class Vertex{
-    constructor(x,y,parent){
-        this.x = x;
-        this.y = y;
+    constructor(parent){
         if(parent){
             this.parent = parent;
             parent.AddChild(this);
@@ -9,42 +7,78 @@ class Vertex{
         this.children = [];
         this.keyframes = [];
     }
+    
+    drawVertex(ctx){
+        ctx.save();
+        ctx.fillStyle = "white";
+        ctx.strokeStyle = "white";
+        for(let i = 0; i < this.children.length; i++){
+            this.children[i].drawVertex(ctx);
+            ctx.beginPath();
+            ctx.moveTo(this.x,this.y);
+            ctx.lineTo(this.children[i].x,this.children[i].y);
+            ctx.stroke();
+            ctx.closePath();
+        }
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,5,0,Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
+        ctx.restore();
+    }
+    
     AddChild(vertex){
         this.children.push(vertex);
     }
     
-    AddKeyframe(xPos,yPos){
+    AddKeyframe(frameData,vertexIndex=0){
         let frame = {
-            x: xPos,
-            y: yPos
+            x: frameData[vertexIndex*2],
+            y: frameData[vertexIndex*2+1]
         };
-//        if(this.keyframes.length-1 <= frameNumber) this.keyframes[frameNumber] = frame;
-//        else this.keyframes.push(frame);
+        vertexIndex++;
         this.keyframes.push(frame);
-    }
-    
-    Interpolate(value=0){
-        this.x = this.LinearInterpolation(this.keyframes[0].x,this.keyframes[1].x,value);
-        this.y = this.LinearInterpolation(this.keyframes[0].y,this.keyframes[1].y,value);
-    }
-    
-    
-    setKeyframe(value){
-        if(this.children.length > 0){
-            for(let i=0;i<this.children.length;i++){
-                this.children[i].setKeyframe(value);
-            }
+        for(let i = 0; i < this.children.length; i++){
+            vertexIndex = this.children[i].AddKeyframe(frameData,vertexIndex);
         }
         
-        if(value < 0.5){
-            this.x = this.LinearInterpolation(this.keyframes[0].x,this.keyframes[1].x,this.normalize(value,0.0,0.5));
-            this.y = this.LinearInterpolation(this.keyframes[0].y,this.keyframes[1].y,this.normalize(value,0.0,0.5));
+        return vertexIndex;
+    }
+    
+    
+    setActiveKeyframe(value){
+        
+        let x0,x1,x2; //0 is lower frame, 1 is real position, 2 is higher frame
+        let y0,y1,y2;
+        
+        if(this.children.length > 0){
+            for(let i=0;i<this.children.length;i++){
+                this.children[i].setActiveKeyframe(value);
+            }
+        }
+        if(value < 128){
+            x0=this.keyframes[0].x;
+            y0=this.keyframes[0].y;
+            
+            x2=this.keyframes[1].x;
+            y2=this.keyframes[1].y;
+            
+            x1 = this.LinearInterpolation(x0,x2,this.normalize(value,0,128));
+            y1 = this.LinearInterpolation(y0,y2,this.normalize(value,0,128));
         }
         else{
-            console.log(this.keyframes[2]);
-            this.x = this.LinearInterpolation(this.keyframes[1].x,this.keyframes[2].x,this.normalize(value,0.5,1.0));
-            this.y = this.LinearInterpolation(this.keyframes[1].y,this.keyframes[2].y,this.normalize(value,0.5,1.0));
+            x0=this.keyframes[1].x;
+            y0=this.keyframes[1].y;
+            
+            x2=this.keyframes[2].x;
+            y2=this.keyframes[2].y;
+            
+            x1 = this.LinearInterpolation(x0,x2,this.normalize(value,128,255));
+            y1 = this.LinearInterpolation(y0,y2,this.normalize(value,128,255));
         }
+        
+        this.x = x1;
+        this.y = y1;
     }
     
     //interpolates y2 between two given points (1 and 3)
@@ -59,12 +93,22 @@ class Vertex{
         return (value-min)/(max-min);
     }
     
-    logVertex(){
-        let rVal = this.x + "," + this.y + ",";
-        for(let i = 0; i < this.children.length; i++){
-            rVal += this.children[i].logVertex();
+    moveTo(newX,newY){
+        let dX = newX - this.x;
+        let dY = newY - this.y;
+        this.x = newX;
+        this.y = newY;
+        for(let i=0;i<this.children.length;i++){
+            this.children[i].translate(dX,dY);
         }
-        return rVal;
+    }
+    
+    translate(dX,dY){
+        this.x += dX;
+        this.y += dY;
+        for(let i=0; i < this.children.length;i++){
+            this.children[i].translate(dX,dY);
+        }
     }
     
     TestProximity(x,y){
@@ -76,35 +120,66 @@ class Vertex{
                 if(!rVal) rVal = this.children[i].TestProximity(x,y);
             }
         }
-        
-        if(dist < 30 && !rVal) rVal = this;
+        if(dist < 10 && !rVal) rVal = this;
+        return rVal;
+    }
+    
+    getKeyframeFromPosition(){
+        let rVal = [];
+        rVal.push(this.x);
+        rVal.push(this.y);
+        for(let i = 0; i < this.children.length; i++){
+            rVal = rVal.concat(this.children[i].getKeyframeFromPosition());
+        }
         return rVal;
     }
 }
 
 class AnimBody{
     constructor(xPos, yPos){
-        this.root = new Vertex(xPos,yPos);
-        this.shoulders = new Vertex(xPos,yPos-70,this.root);
+        this.root = new Vertex();
+        this.pelvis = new Vertex(this.root);
+        this.shoulders = new Vertex(this.pelvis);
         
-        this.leftElbow = new Vertex(this.shoulders.x - 40,this.shoulders.y,this.shoulders);
-        this.leftHand = new Vertex(this.leftElbow.x - 30,this.leftElbow.y,this.leftElbow);
-        this.rightElbow = new Vertex(this.shoulders.x + 40,this.shoulders.y,this.shoulders);
-        this.rightHand = new Vertex(this.rightElbow.x + 30,this.rightElbow.y,this.rightElbow);
+        this.leftElbow = new Vertex(this.shoulders);
+        this.leftHand = new Vertex(this.leftElbow);
+        this.rightElbow = new Vertex(this.shoulders);
+        this.rightHand = new Vertex(this.rightElbow);
         
-        this.head = new Vertex(this.shoulders.x,this.shoulders.y - 40,this.shoulders);
+        this.head = new Vertex(this.shoulders);
         
-        this.leftKnee = new Vertex(this.root.x-30,this.root.y+30,this.root);
-        this.leftFoot = new Vertex(this.leftKnee.x-20,this.leftKnee.y+30,this.leftKnee);
-        this.rightKnee = new Vertex(this.root.x+30,this.root.y+30,this.root);
-        this.rightFoot = new Vertex(this.rightKnee.x+20,this.rightKnee.y+30,this.rightKnee);
-        
+        this.leftKnee = new Vertex(this.pelvis);
+        this.leftFoot = new Vertex(this.leftKnee);
+        this.rightKnee = new Vertex(this.pelvis);
+        this.rightFoot = new Vertex(this.rightKnee);
         
         this.activeVertex = null;
         
-        this.setGrossLow();
-        this.setGrossMed();
-        this.setGrossHigh();
+        this.position = {
+            x: xPos,
+            y: yPos 
+        };
+        
+        //get low keyframe from HTML (because JS is a constant, losing battle), parse into int array
+        let kf = document.getElementById("keyframeLow").innerHTML.split(',');
+        for(let i=0; i < kf.length;i++){
+            kf[i] = parseInt(kf[i]);
+        }
+        this.root.AddKeyframe(kf);
+        
+        //get medium keyframe
+        kf = document.getElementById("keyframeMed").innerHTML.split(',');
+        for(let i=0; i < kf.length;i++){
+            kf[i] = parseInt(kf[i]);
+        }
+        this.root.AddKeyframe(kf);
+        
+        //get high keyframe
+        kf = document.getElementById("keyframeHigh").innerHTML.split(',');
+        for(let i=0; i < kf.length;i++){
+            kf[i] = parseInt(kf[i]);
+        }
+        this.root.AddKeyframe(kf);
     }
     
     //set gross functions are just a proof of concept. Not the final way of inputing keyframes but I'm low on time for this prototype and javascript is really being a javascript right now
@@ -158,39 +233,26 @@ class AnimBody{
     }
     
     setActiveVertex(x,y){
-        this.activeVertex = this.root.TestProximity(x,y);
+        this.activeVertex = this.pelvis.TestProximity(x,y);
     }
     
-    //temp function because Javascript is a nightmare generator
-    logKeyframe(){
-        console.log(this.root.logVertex());
+    setActiveKeyframe(value){
+        this.root.setActiveKeyframe(value);
     }
     
-    setKeyframe(value){
-        this.root.setKeyframe(value);
+    setKeyframe(frameNum){
+        
     }
     
     draw(ctx){
-        this.drawVertex(this.root,ctx);
+        ctx.save();
+        ctx.translate(this.position.x,this.position.y);
+        this.pelvis.drawVertex(ctx);
+        ctx.restore();
     }
     
-    drawVertex(v,ctx){
-        ctx.save();
-        ctx.fillStyle = "white";
-        ctx.strokeStyle = "white";
-        for(let i = 0; i < v.children.length; i++){
-            this.drawVertex(v.children[i],ctx);
-            ctx.beginPath();
-            ctx.moveTo(v.x,v.y);
-            ctx.lineTo(v.children[i].x,v.children[i].y);
-            ctx.stroke();
-            ctx.closePath();
-        }
-        ctx.beginPath();
-        ctx.arc(v.x,v.y,5,0,Math.PI * 2);
-        ctx.fill();
-        ctx.closePath();
-        ctx.restore();
+    logFrame(){
+        console.log(this.root.logKeyframe());
     }
 }
 
